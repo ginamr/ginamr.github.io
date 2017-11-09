a566bd3560915aca06a28e7056675465904b2095
Gina <ginamr@uci.edu>
2017-11-09 14:21:34 -0800

    breakpoints added for text rescaling, need to fix imgs

---
+++ Scripts/headshot.js
@@ -0,0 +1,9 @@
+function tellSize() {
+	//var img = document.getElementById('iconDimensions');
+	//alert(img.width + 'x' + img.height);
+	//alert("hi");
+	var test = document.querySelector("li#iconDimensions");
+	alert(test);
+	
+};
+window.onload = tellSize();
