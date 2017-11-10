88b943c60386d566fc2b33f535d9aa342d6b852b
Gina <ginamr@uci.edu>
2017-11-09 17:34:51 -0800

    basic other pgs now

--- Scripts/motion.js
+++ Scripts/motion.js
@@ -1,13 +1,3 @@
 function scrollToTop() {
 	window.scrollTo(0, 0);
 };
-
-/*function tellSize() {
-	//var img = document.getElementById('iconDimensions');
-	//alert(img.width + 'x' + img.height);
-	//alert("hi");
-	var test = document.querySelector("li#iconDimensions");
-	alert(test);
-	
-};
-window.onload = tellSize();*/
