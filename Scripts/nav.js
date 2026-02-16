32cb5d260fc0eb8b52da928b424e721a3ce3c806
Gina Robinson <gina.robinson@cox.net>
2026-02-16 13:37:22 -0800

    tabs to spaces common nav add test

---
+++ Scripts/nav.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navHTML = `
+        <nav>
+            <ul>
+                <li id="logo">
+                    <a href="index.html">
+                        <img class="icon" src="Assets/gr.svg" alt="GR" style="float: right;">
+                    </a>				
+                </li>
+                <li><a href="MS2017.html">MS 2017</a></li>
+                <li><a href="MS2016.html">MS 2016</a></li>
+                <li><a href="fablix.html">Fablix</a></li>
+                <li><a href="bailys-beads.html">Baily's Beads</a></li>
+            </ul>
+        </nav>
+    `;
+    const temp = document.createElement('div');
+    temp.innerHTML = navHTML;
+    document.body.insertAdjacentElement('afterbegin', temp.firstElementChild);
+});
