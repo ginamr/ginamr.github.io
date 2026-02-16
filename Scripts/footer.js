9de2a533f7d1b83be1197b68d3f6b4d85bc8a589
Gina Robinson <gina.robinson@cox.net>
2026-02-16 14:59:53 -0800

    common footer

---
+++ Scripts/footer.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const footerHTML = `
+        <footer>
+            <div id="email" class="project-img">
+                <img src="Assets/email.png" alt="helloginarobinson at gmail dot com">
+            </div>
+            <p>helloginarobinson at gmail dot com</p>
+            <p><small class="copyright">&copy;2026 Gina Marie Robinson</small></p>
+        </footer>
+    `;
+    const temp = document.createElement('div');
+    temp.innerHTML = footerHTML;
+    document.body.insertAdjacentElement('beforeend', temp.firstElementChild);
+});
