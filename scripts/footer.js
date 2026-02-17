2745cb62a4dd1d096ade78b731586002fd521065
Gina Robinson <gina.robinson@cox.net>
2026-02-17 00:30:30 -0800

    png size

--- scripts/footer.js
+++ scripts/footer.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
     const footerHTML = `
         <footer>
-            <div id="email" class="project-img">
+            <div id="email">
                 <img src="${pathPrefix}assets/email.png" alt="helloginarobinson at gmail dot com">
             </div>
             <p>toginarobinson at gmail dot com</p>
